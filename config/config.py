6e77b939311d7359688be82a1d24a9b6307befd5
Gutes Tod <jefflol003@gmail.com>
2023-04-13 22:44:52 +0500

    Написать парсер конфигов Fixes #2

---
+++ config/config.py
@@ -0,0 +1,13 @@
+import os
+from configparser import ConfigParser
+
+CONFIG_DIR = os.path.dirname(__file__)
+
+class Config():
+    def __init__(self, path):
+        self.cfg = ConfigParser()
+        self.cfg.read(path) # извлекаются данные из .ini файла
+    def ConfigTelegram(self):
+        return self.cfg['API_KEY']
+
+cfg = Config(CONFIG_DIR + '\config.ini')
